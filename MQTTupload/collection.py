ea495adb22312014ee254a8d0ccaaaa1985ff323
Steve <steve@rata.co.nz>
2017-09-02 21:10:12 +1200

    Progress on multiple nodes

--- MQTTupload/collection.py
+++ MQTTupload/collection.py
@@ -1,16 +1,31 @@
-switch_links = {'Core224' : ['Fanout225', 'Trans218','PoE226', 'Link202', 'Link203', 'Link204', 'Link205',
-                         'Fanout225', 'Matai310', 'DTS311','PLink219', 'Exta207', 'Juno216', 'Dploy228', 'Trans218' ],
-        'Trans218' : ['LARCC301'],
-        'LARCC301' : ['Juno302', 'Juno303'],
-        'Juno216' : ['Juno304'],
-        'Juno304' : ['Cisco305'],
-        'DTS311' : ['DDMZ312'],
-        'Matai310' : ['PDMZ321'],
-        'Fanout225' : ['VoIP201'],
-        'PLink219' : ['Atwo501'],
+# Sort out symbols
+# http://www.utf8-chartable.de/unicode-utf8-table.pl?utf8=0x&unicodeinhtml=hex
+POWER3 = u'\xb3'
+DEG = u'\xb0'  # utf code for degree
+# ENCODING = 'UTF-8'
+# print DEG
+
+
+SENSOR_NODES = {
+	'A' : [ 'Temp', 'Temperature', DEG, 'degrees celcius' ],
+        'B' : [ 'Humid', 'Humidity', '%', '%' ],
+        'C' : [ 'Rain', 'Rainfall', 'millimetres' ],
+        'D' : [ 'BaroP', 'Barametric Pressure', 'hPa', 'hectopascal' ],
+        'E' : [ 'Capacitance', 'Capacitance', 'F', 'farad' ],
+        'F' : [ 'Wght', 'Weight', 'g', 'grammes' ],
+        'G' : [ 'Light', 'Light', 'lx', 'lux' ],
+        'H' : [ 'Density', 'Density (mass)', 'g/cm'+POWER3, 'grammes per cubic centimetre' ],
+        'I' : [ 'NodeI', 'Node I sensor data', 'I', 'Units of node I' ],
+        'J' : [ 'NodeJ', 'Node J sensor data', 'J', 'Units of node J' ],
                         }
 
-for swl, links in switch_links.iteritems():
-        print (swl , links)
-        for sw2 in links:
-		print (swl, links, sw2)
+for NODE, UNITS in SENSOR_NODES.iteritems():
+	print NODE, ": '",  
+	for UNIT in UNITS:
+		print  UNIT, "'", 
+	print 
+
+
+#        print (swl , links)
+#        for sw2 in links:
+#		print (swl, links, sw2)
