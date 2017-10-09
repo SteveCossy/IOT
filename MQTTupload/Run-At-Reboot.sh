68e67ae44adf9b003ef86d3082625e4bfbaa4bb3
Steve <steve@rata.co.nz>
2017-10-10 09:10:12 +1300

    Autostarting using cron and Run at startup

--- MQTTupload/Run-At-Reboot.sh
+++ MQTTupload/Run-At-Reboot.sh
@@ -5,7 +5,7 @@ echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\
 echo -n About to start at: >>$OUTFILE
 date >>$OUTFILE
 
-sleep 60
+sleep 120
 
 /usr/bin/python3 /home/pi/IOT/MQTTupload/Serial_multi_MQTT3.py >>$OUTFILE 2>>$OUTERROR 
 
