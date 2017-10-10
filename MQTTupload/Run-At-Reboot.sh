426bd745cab3d64ed6c12ef299cfe308c05cc8bb
Steve <steve@rata.co.nz>
2017-10-10 23:48:08 +1300

    Run at startup to check for webiopi and ping

--- MQTTupload/Run-At-Reboot.sh
+++ MQTTupload/Run-At-Reboot.sh
@@ -5,7 +5,24 @@ echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\
 echo -n About to start at: >>$OUTFILE
 date >>$OUTFILE
 
-sleep 120
+# Wait for webiopi to get started
+while [ `ps -ef | grep -v grep | grep -c webiopi` -eq 1 ] ; do sleep 1 ; done
+
+echo -n webiopi running: >>$OUTFILE
+date >>$OUTFILE
+
+# Wait till networking is working
+WAITING=true
+
+while $WAITING do
+	if ping mydevices.com -c 1 >\dev\nul 2>\$OUTFILE
+		then WAITING=false
+	fi
+	sleep 1
+done
+
+echo -n ping working: >>$OUTFILE
+date >>$OUTFILE
 
 /usr/bin/python3 /home/pi/IOT/MQTTupload/Serial_multi_MQTT3.py >>$OUTFILE 2>>$OUTERROR 
 
