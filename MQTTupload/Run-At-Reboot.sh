8813593bda8c994b8d731dd733b4967c0f200489
Steve Cosgrove <SteveCossy@users.noreply.github.com>
2017-10-11 08:36:05 +1300

    Lined up the diagnostic messages nicely

--- MQTTupload/Run-At-Reboot.sh
+++ MQTTupload/Run-At-Reboot.sh
@@ -8,7 +8,7 @@ date >>$OUTFILE
 # Wait for webiopi to get started
 while [ `ps -ef | grep -v grep | grep -c webiopi` -eq 0 ] ; do sleep 1 ; done
 
-echo -n webiopi running:   >>$OUTFILE
+echo -n webiopi running  : >>$OUTFILE
 date >>$OUTFILE
 
 # Wait till networking is working
@@ -22,7 +22,7 @@ do
 	sleep 1
 done
 
-echo -n ping working:      >>$OUTFILE
+echo -n ping working     : >>$OUTFILE
 date >>$OUTFILE
 
 /usr/bin/python3 /home/pi/IOT/MQTTupload/Serial_multi_MQTT3.py >>$OUTFILE 2>>$OUTERROR &
