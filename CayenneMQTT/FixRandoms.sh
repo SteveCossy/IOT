fcc772cba1e3d9d2d8e530743dd0aba65d8dfb06
Steve <steve@rata.co.nz>
2019-11-28 22:30:27 +1300

    Saved files needed to edit csv file

---
+++ CayenneMQTT/FixRandoms.sh
@@ -0,0 +1,6 @@
+  739  nano +708  /home/cosste/RSSILatLong.csv
+  740  nano +943  /home/cosste/RSSILatLong.csv
+  741  history | tail -4 
+  742  echo $HOME
+  743  history | tail -6
+  744  history | tail -6 > IOT/CayenneMQTT/FixRandoms.sh
