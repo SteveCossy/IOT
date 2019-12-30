27d1f8af5ccd2cc1b9fa1e62848a03870ddd2747
shermp <14854761+shermp@users.noreply.github.com>
2019-12-31 11:15:13 +1300

    Create to_geojson function in csv2json.py
    So that the code can be imported into form.py

--- webpy/csv2json.py
+++ webpy/csv2json.py
@@ -2,12 +2,11 @@
 # from https://stackoverflow.com/questions/48586647/python-script-to-convert-csv-to-geojson
 # started 17 November 2019
 # modified to take csv from command line 19 Dec 2019
+# modified to encapsulate main logic into function 31 Dec 2019
 
 import csv, sys, os, json, webbrowser
 from collections import OrderedDict
 
-InputFile=str(sys.argv[1])
-OutputFile=str(sys.argv[2]) # This is currently a full path and filename
 # InputPath ='./archive.old'
 # InputPath ='/home/cosste/CayMQTT/'
 # OutputPath='/var/www/html/OSM/temp/'
@@ -30,38 +29,49 @@ OutputFile=str(sys.argv[2]) # This is currently a full path and filename
 # os.system('scp 192.168.196.71:/home/cosste/RSSILatLong.csv '+InputFile)
 # Got get the csv file from the computer where it is created
 
-li = []
-with open(InputFile, 'r') as CsvFile:
-#    dialect = csv.Sniffer().sniff(CsvFile.read(1024))
-    reader = csv.reader(CsvFile, delimiter=',')
-    next(reader) # skip header
-#    reader = csv.reader(CsvFile, dialect)
-    for TIME,RSSI,LAT,LONG  in reader:
-        d = OrderedDict()
-        d['type'] = 'Feature'
-        d['properties'] = {
-            'TimeStamp': TIME,
-            'RSSI': RSSI,
-            'Lat' : LAT,
-            'Long': LONG
-        }
-        d['geometry'] = {
-            'type': 'Point',
-            'coordinates': [float(LONG), float(LAT)]
-#            'coordinates': [Latitude, Longitude]
-        }
-        li.append(d)
+# Turn this into a function that can be imported from another script -SP
+def to_geojson(InputFile, OutputFile):
+    """Convert CSV file to GeoJSON"""
 
-# print( 'Writing '+OutputFile )
+    li = []
+    with open(InputFile, 'r') as CsvFile:
+    #    dialect = csv.Sniffer().sniff(CsvFile.read(1024))
+        reader = csv.reader(CsvFile, delimiter=',')
+        next(reader) # skip header
+    #    reader = csv.reader(CsvFile, dialect)
+        for TIME,RSSI,LAT,LONG  in reader:
+            d = OrderedDict()
+            d['type'] = 'Feature'
+            d['properties'] = {
+                'TimeStamp': TIME,
+                'RSSI': RSSI,
+                'Lat' : LAT,
+                'Long': LONG
+            }
+            d['geometry'] = {
+                'type': 'Point',
+                'coordinates': [float(LONG), float(LAT)]
+    #            'coordinates': [Latitude, Longitude]
+            }
+            li.append(d)
 
-d = OrderedDict()
-d['type'] = 'FeatureCollection'
-d['features'] = li
-with open(OutputFile, 'w') as f:
-    f.write(json.dumps(d, sort_keys=False, indent=4))
+    # print( 'Writing '+OutputFile )
+
+    d = OrderedDict()
+    d['type'] = 'FeatureCollection'
+    d['features'] = li
+    with open(OutputFile, 'w') as f:
+        f.write(json.dumps(d, sort_keys=False, indent=4))
 
 # webbrowser.open('http://net-informations.com', new=2)
 
 # NewURL = 'http://students.pcsupport.ac.nz/OSM/?'+OutputFile
 # os.system("echo "+NewURL+" >NewURL.url")
 # webbrowser.open(NewURL, new=2)
+
+# This code only gets called when the file is called on the command line
+# It does not run if imported into another script -SP
+if __name__ == '__main__':
+    InputFile=str(sys.argv[1])
+    OutputFile=str(sys.argv[2]) # This is currently a full path and filename
+    to_geojson(InputFile, OutputFile)
