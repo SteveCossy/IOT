d37607f2c570cde176877e1aca58dfd0295ed7c6
Steve Cosgrove <steve@rata.co.nz>
2020-03-15 19:13:13 +1300

    Added example command line to display wifi stats

--- test/WiFiLevel.sh
+++ test/WiFiLevel.sh
@@ -1,6 +1,7 @@
 # https://linux.die.net/man/8/iwconfig
 # https://linuxize.com/post/bash-while-loop/
 # /home/pi/IOT/test/WiFiLevel.sh Sunday-Open-Again | tee -a ~/wlan0-stats.log &
+# while : ; do cat /proc/net/wireless ; sleep ; done
 
 if [ -z "$2" ]
 then
