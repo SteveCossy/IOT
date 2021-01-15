8ac10822deb0a94de99428c62c044e3e1be6820d
Steve Cosgrove <steve@rata.co.nz>
2021-01-15 18:36:42 +1300

    Fixing Python switchoff

--- LoRaReAd/switchOff.py
+++ LoRaReAd/switchOff.py
@@ -1,9 +1,14 @@
 # Force GPIO 14 low to tell an attached PicAxe to shut off power soon
 from gpiozero import LED
 import os #imports OS library for Shutdown control
+import time
 
 # Based on https://gpiozero.readthedocs.io/en/stable/recipes.html
 indicator = LED(14)
-indicator.off()
+start_time = time.time()
+
+# Take down GPIO 14 and keep it off for 5 seconds
+while time.time()<start_time+8
+   indicator.off()
 
 os.system("sudo poweroff") #shut down the Pi -h is or -r will reset
---
+++ bash/switchOff.sh
@@ -0,0 +1,1 @@
+python3 /home/pi/IOT/LoRaReAd/switchOff.py
