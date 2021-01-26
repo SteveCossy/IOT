56ee7b9fde9accab3556ecd05b50d5b5a0df1eb4
Steve Cosgrove <steve@rata.co.nz>
2021-01-26 05:14:05 +0000

    Got SerialTest.py working (to a degree)

--- test/SerialTest.py
+++ test/SerialTest.py
@@ -1,11 +1,17 @@
+import os, sys
+
+HomeDir =       os.environ['HOME']
+# the IOT/LoRaReAd dir contains MQTTUtils.py
+MQTTUpath =     os.path.join(HomeDir,'IOT/LoRaReAd')
+sys.path.append(MQTTUpath)
 from MQTTUtils import PiSerial
 
 serialDevices = PiSerial()
 
-print('Onboard port is:'+serialDevices['Onboard'] )
+# print('Onboard port is:'+serialDevices['Onboard'] )
 print('USB port(s):',end='')
 for Device in serialDevices.keys() :
-    if 'USB' in Device:
+#    if 'USB' in Device:
        print(Device,end=',')
 print()
 
