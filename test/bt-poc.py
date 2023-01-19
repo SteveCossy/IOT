546b67a4cce6e404178ec918878a8f0ed8ff61c0
Steve Cosgrove <steve@rata.co.nz>
2023-01-19 23:05:30 +1300

    Actual data out of  BT Python

--- test/bt-poc.py
+++ test/bt-poc.py
@@ -9,6 +9,13 @@ import socket
 s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 s.connect(('00:18:E4:34:E7:08', 1))
 
-print (s.recv(1024))
+Run_flag=True # Get the loop started
+
+while Run_flag :
+    try:  # catch a <CTRL C>
+        out = s.recv(1024)
+        print (out)
+    except KeyboardInterrupt:
+        Run_flag=False # Stop the loop
 
 
