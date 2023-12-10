60120fb21d22f4cf65e6987538f136276f0825b9
Steve Cosgrove <steve@rata.co.nz>
2023-12-10 01:32:47 +0000

    Version 0.1-debug

--- MQTT-zafron/NgāMotu-Zafron.py
+++ MQTT-zafron/NgāMotu-Zafron.py
@@ -12,7 +12,7 @@ import paho.mqtt.client as mqtt
 # Useful constants
 HomeDir = 	os.environ['HOME']
 # HomeDir	= '/home/pi'
-ConfPathFile = 'MQTT-Zafron.txt'
+ConfPathFile = '/MQTT-Zafron.txt'
 
 CsvPath = os.path.join(HomeDir,ConfPathFile)
 CSV 	= '.csv'
